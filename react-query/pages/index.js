5cff8223c80b343e78a3058058271d2da9edd3ad
江波戸航平 <k-ebato@sensyn-robotics.com>
2022-09-03 19:53:12 +0900

    move constans to lib becuase it has no react default func

--- react-query/pages/index.js
+++ react-query/pages/index.js
@@ -8,7 +8,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { queryOptions } from './constant'
+import { queryOptions } from '../lib/constant'
 
 const queryClient = new QueryClient()
 
--- react-query/pages/projects/[id].js
+++ react-query/pages/projects/[id].js
@@ -9,7 +9,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { queryOptions } from '../constant';
+import { queryOptions } from '../../lib/constant';
 
 const queryClient = new QueryClient()
 
